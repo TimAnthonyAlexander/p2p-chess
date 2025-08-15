5f3ca985b595085e7bbfeb97650123e73bed594d
Tim Anthony Alexander <info@timanthonyalexander.de>
2025-08-15 13:55:16 +0200

    Env

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,9 +7,15 @@ import (
 
 	apihttp "p2p-chess/internal/http"
 	"p2p-chess/internal/store"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file: ", err)
+	}
+
 	dsn := os.Getenv("DB_DSN")
 	if dsn == "" {
 		log.Fatal("DB_DSN not set")
